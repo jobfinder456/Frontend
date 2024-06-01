d7cc5a08bcdfdb87282674c0d16e41d2f5aa8b8f
Sahil <sk2003311@gmail.com>
2024-06-01 15:53:12 +0530

    fix address

--- src/app/contact-address/page.jsx
+++ src/app/contact-address/page.jsx
@@ -10,12 +10,29 @@ const page = () => {
       <div className="max-w-[54.75rem] mx-auto p-[1rem]">
         <div className=" bg-background rounded-[1rem] p-[1rem] flex flex-col gap-[1rem] my-[2rem] ">
           <p className="text-[1.2rem]">
+            For any queries, feel free to reach out to us at{" "}
+            <a
+              href="mailto:hq@getjobs.today"
+              className="text-base-2 underline font-medium"
+            >
+              hq@getjobs.today
+            </a>
+            . We usually respond within 12 hours.
+          </p>
+          <p className="text-[1.2rem]">
             Office Address{" - "}
             <span className="font-medium">
               P2 Belegata Road, Near Sales Tax Office, Kolkata - 700015, West
               Bengal
             </span>
           </p>
+
+          <Link
+            href={"mailto:hq@getjobs.today"}
+            className="button-primary text-center mt-[2rem]"
+          >
+            Shoot us your query
+          </Link>
         </div>
       </div>
     </div>
